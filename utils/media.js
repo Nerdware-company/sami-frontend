7f469f81e08ce37df9ae9c64ef0d3bee19481506
ahmadmubarak98 <mubarak.ahmad@outlook.com>
2021-11-20 11:02:25 +0200

    sd

--- utils/media.js
+++ utils/media.js
@@ -5,12 +5,11 @@ export function getStrapiMedia(url) {
 
   // Return the full URL if the media is hosted on an external provider
   if (url.startsWith("http") || url.startsWith("//")) {
-    return `https://cors-anywhere.herokuapp.com/http:${url}`;
+    return url;
   }
 
   // Otherwise prepend the URL path with the Strapi URL
   return `${
-    process.env.NEXT_PUBLIC_STRAPI_API_URL ||
-    "https://cors-anywhere.herokuapp.com/http://161.97.161.196:1337"
+    process.env.NEXT_PUBLIC_STRAPI_API_URL || "http://161.97.161.196:1337"
   }${url}`;
 }
